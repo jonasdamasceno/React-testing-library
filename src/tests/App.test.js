51332aa5ec47e7b748d883f6bf63a7e255c717c2
JonasDamasceno <jonasmichael2021@outlook.com>
2022-10-19 14:32:21 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('App', () => {
+  test('testa se ha um link com o texto "home" na tela', () => {
+    renderWithRouter(<App />);
+    const linkElement = screen.getByRole('link', { name: /home/i });
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('testa se ha um link com o texto "about" na tela', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkAbout).toBeInTheDocument();
+  });
+  test('testa se ha um link com o texto "Favorite Pokemons" na tela', () => {
+    renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite pokémons/i });
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
+
