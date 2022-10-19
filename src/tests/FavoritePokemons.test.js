e63d51e24f97fe95d77597cc54f8124a4614b46d
JonasDamasceno <jonasmichael2021@outlook.com>
2022-10-19 18:23:17 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('testa FavoritePokemons', () => {
+  it('testa se há na tela o texto "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const text = screen.getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+});
